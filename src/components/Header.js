6419eda39e5b9c812b71c2a36cad6a3f7f8957e5
Folashy <100294216+Folashy@users.noreply.github.com>
2022-11-13 20:22:49 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Meta from '../images/Group.svg'
+
+
+function Header() {
+  return (
+      <div>
+        <div>
+          <img src={Meta} alt='Metabnb' />
+          <div>Home</div>
+          <div>Place to stay</div>
+          <div>NFT's</div>
+          <div>Community</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
+
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+      <div>LandingPage
+           <img src={header} alt='Metabnb'/>
+    </div>
+  )
+}
+
+export default LandingPage
