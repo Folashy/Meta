d2c3cb17619b9a9ee7e380f24481d33328f40524
Folashy <100294216+Folashy@users.noreply.github.com>
2022-11-14 14:08:19 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import './index.css';
+import React from 'react';
 import Header from './components/Header';
-import Home from './pages/Home';
+import Home from './components/Home';
+import Footer1 from './components/Footer1'
+import Footer2 from './components/Footer2'
+
+
+import './index.css';
+
 
 function App() {
   return (
     <div>
-      <Header />
+     
+     <Header />
       <Home />
+      <Footer1 />
+      <Footer2 />
     </div>
   );
 }
